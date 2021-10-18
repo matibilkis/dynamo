35a733bb0f165daae12a9d274af3dbe0e49b4baf
matias.bilkis@uab.cat <mbilkis@td646.pic.es>
2021-10-18 13:26:56 +0200

    results dolinar 2 phases obtained at PIC

--- .ipynb_checkpoints/meta_main-checkpoint.py
+++ .ipynb_checkpoints/meta_main-checkpoint.py
@@ -2,7 +2,7 @@ import time
 import os 
 import subprocess
 
-for l in range(1,6,3):
+for l in range(1,12,3):
     lmin = l
     lmax = l+3
 
@@ -20,12 +20,10 @@ for l in range(1,6,3):
 
         f"L={L}\n"\
         f"args=$(Item) $(L)\n"\
-        f"queue from seq 10.01 0.01 10.5| \n"
+        f"queue from seq 0.01 0.01 1.5| \n"
 
         file.write(info)
 
     os.system("chmod +x mcondor.sub")
-    os.system('GREPDB="condor_submit mcondor.sub"; /bin/bash -c "$GREPDB"')
-#    os.system("condor_submit mcondor.sub")
-    #subprocess.call('/bin/bash -c "$GREPDB"', shell=True, env={'GREPDB': 'condor_submit mcondor.sub'})
+    os.system("condor_submit mcondor.sub")
     time.sleep(1)
