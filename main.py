f93cc4dbcc6537d6da312f9a39c51fab162d513b
matias.bilkis@uab.cat <mbilkis@tds202.pic.es>
2021-10-18 17:24:53 +0200

    inspecting actions

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ from scipy import interpolate
 import argparse
 import os
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("--alpha", type=float, default=0.4)
-parser.add_argument("--L", type=int, default=3)
-parser.add_argument("--path", type=str, default="/data/uab-giq/scratch/matias/dynamo/")
-
-args = parser.parse_args()
-alpha = args.alpha
-L=args.L
 
 def P_0(eta):
     return np.max([eta, 1-eta])
@@ -61,43 +53,53 @@ def give_bounds(eta, alpha):
     else:
         return [((-1.5*alpha,1.5*alpha))]
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--alpha", type=float, default=0.4)
+    parser.add_argument("--L", type=int, default=3)
+    parser.add_argument("--path", type=str, default="/data/uab-giq/scratch/matias/dynamo/")
 
-etas_min = np.linspace(0,.5,100)
-whole_etas = np.concatenate([etas_min, (1-etas_min)[::-1]])
+    args = parser.parse_args()
+    alpha = args.alpha
+    L=args.L
 
-PS = np.zeros((L,2*len(etas_min)))
-BOPT = np.zeros((L,2*len(etas_min)))
 
-for el in range(L)[::-1]:
-    print(el, alpha)
-    if el==L-1:
-        for ind_eta, eta in enumerate(tqdm(etas_min)):
-            if ind_eta == 0:
-                PS[el, ind_eta] = 0
-                BOPT[el,ind_eta]= alpha
-            else:
-                optimization = optimize.dual_annealing(P_1, x0=np.array([ BOPT[el, ind_eta-1]]), args=([eta], [alpha/np.sqrt(L)]), bounds=give_bounds(eta, alpha))
-                PS[el,ind_eta]=optimization.fun
-                BOPT[el,ind_eta]=optimization.x
-        PS[el,len(etas_min):] = PS[el,:len(etas_min)][::-1]
-        BOPT[el,len(etas_min):] = -BOPT[el,:len(etas_min)][::-1]
+    etas_min = np.linspace(0,.5,100)
+    whole_etas = np.concatenate([etas_min, (1-etas_min)[::-1]])
 
-    else:
-        model = interpolate.Rbf(whole_etas, PS[el+1], smooth=0.01, epsilon=1e-2)
-        for ind_eta, eta in enumerate(tqdm(etas_min)):
+    PS = np.zeros((L,2*len(etas_min)))
+    BOPT = np.zeros((L,2*len(etas_min)))
 
-            if ind_eta == 0:
-                PS[el, ind_eta] = 0
-                BOPT[el,ind_eta]= alpha
-            else:
-                optimization  = optimize.dual_annealing(P_n, x0=np.array([ BOPT[el, ind_eta-1]]), args=(eta, model, 1/np.sqrt(L)), bounds=give_bounds(eta, alpha))
-                PS[el,ind_eta]=optimization.fun
-                BOPT[el,ind_eta]=optimization.x
-        PS[el,len(etas_min):] = PS[el,:len(etas_min)][::-1]
-        BOPT[el,len(etas_min):] = -BOPT[el,:len(etas_min)][::-1]
-    print("LAYER {}/{} DONE! \n".format(el+1, L))
+    for el in range(L)[::-1]:
+        print(el, alpha)
+        if el==L-1:
+            for ind_eta, eta in enumerate(tqdm(etas_min)):
+                if ind_eta == 0:
+                    PS[el, ind_eta] = 0
+                    BOPT[el,ind_eta]= alpha
+                else:
+                    optimization = optimize.dual_annealing(P_1, x0=np.array([ BOPT[el, ind_eta-1]]), args=([eta], [alpha/np.sqrt(L)]), bounds=give_bounds(eta, alpha))
+                    PS[el,ind_eta]=optimization.fun
+                    BOPT[el,ind_eta]=optimization.x
+            PS[el,len(etas_min):] = PS[el,:len(etas_min)][::-1]
+            BOPT[el,len(etas_min):] = -BOPT[el,:len(etas_min)][::-1]
 
-name = args.path+"/{}/{}".format(L,alpha)
-os.makedirs(name, exist_ok=True)
-np.save(name+"/Ps", PS)
-np.save(name+"/Bo", BOPT)
+        else:
+            model = interpolate.Rbf(whole_etas, PS[el+1], smooth=0.01, epsilon=1e-2)
+            for ind_eta, eta in enumerate(tqdm(etas_min)):
+
+                if ind_eta == 0:
+                    PS[el, ind_eta] = 0
+                    BOPT[el,ind_eta]= alpha
+                else:
+                    optimization  = optimize.dual_annealing(P_n, x0=np.array([ BOPT[el, ind_eta-1]]), args=(eta, model, 1/np.sqrt(L)), bounds=give_bounds(eta, alpha))
+                    PS[el,ind_eta]=optimization.fun
+                    BOPT[el,ind_eta]=optimization.x
+            PS[el,len(etas_min):] = PS[el,:len(etas_min)][::-1]
+            BOPT[el,len(etas_min):] = -BOPT[el,:len(etas_min)][::-1]
+        print("LAYER {}/{} DONE! \n".format(el+1, L))
+
+    name = args.path+"/{}/{}".format(L,alpha)
+    os.makedirs(name, exist_ok=True)
+    np.save(name+"/Ps", PS)
+    np.save(name+"/Bo", BOPT)
