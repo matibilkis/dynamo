c98928fe7d33c0965f17962e203f9543c42a751c
matibilkis <matiasbilkis@gmail.com>
2021-10-19 13:07:32 +0200

    channel, but not working in pic

--- .ipynb_checkpoints/subash-checkpoint.sh
+++ .ipynb_checkpoints/subash-checkpoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 alpha=$1
 L=$2
+chparam=$3
 cd ~/dynamo
 . ~/qenv_bilkis/bin/activate
-python3 main.py --alpha $alpha --L $L
+python3 main.py --alpha $alpha --L $L --Npriors 10 --chanel_params $chparam 1 
 deactivate
